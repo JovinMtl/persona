527813dd38b66d5d5112f984b8d94dbd7953f5ab
JovinMtl <nsanzumukizath@gmail.com>
2024-04-09 12:27:05 +0200

    the summary is set

--- src/views/auxiliaire/summary.vue
+++ src/views/auxiliaire/summary.vue
@@ -1,5 +1,46 @@
 <template>
-    <div>
+    <div class="summary">
         This is my summary
+        <h3>Who is Thierry ?</h3>
+        <div class="summaryContent" style="text-align: justify; text-indent: 2em;">
+            <p>
+                He is a Burundian Freelancer, studied Computer Science at ETS Kamenge  and graduated in 2016. 
+                First wrote his "hello world" in 2013. Forth in 2014, he discovered C programming language from the computer science course.  
+                Got addicted on Linux the same year, he could jump some course whereas reading Linux book in the library. 
+                He learned to Vim in 2015 and various UNIX commands as well as Bash scripting. 
+            </p>
+            <p>
+                Thanks to the Successful collaboration with his utmost friend Jonathan NKURUNZIZA(Github: INGANZAMARUMPU) 
+                since their first meeting in 2012 at the start of their studies, The friend has contributed the unleashed help 
+                and motivation to Thierry into computer science.
+            </p>
+            <p>
+                After graduation in 2016, He worked on a big project which was written in C programming language in 2017. 
+                It is on that project that he was overwhelmed with the power on "POINTERS". 
+                He felt the joy of the feeling of touching the computer memory as he were entrusted such high responsibility( :-)reserved only and only for C/C++ programmers).
+            </p>
+            <p>
+                However, it turned to be in 2018 till 2021 that he switched into studying "Trade and Marketing" at University of Burundi. 
+                He needed this field to compensate the skills he needed. During that pursuit, He discovered interesting areas such as : 
+                Ethics, "Economie politique", "Comportement du Consommateur", Citizenship Values, "Administration Publique", Business&Administration Writings, 
+                "Principe du Marketing", and more related to Interpersonal Skills.
+                He felt also that he need to do something about "Developpement Personnel" as it is ongoing process of interconnecting 
+                his first Computer science's career and  Business Management. 
+            </p>
+            <p>
+                Thus He is able to respond to the call of Freelancing as  a  Business Strategist & Fullstack Developer.
+            </p>
+        </div>
+        
     </div>
 </template>
+<style scoped>
+.summary{
+    display: inline-block;
+    position: relative;
+    text-align: center;
+    width: 90vw;
+    height: 40px;
+    border-radius: 25px;
+}
+</style>
