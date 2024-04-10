8577b0acc3c6c5c964d1fceacfec41c01c5fbf67
JovinMtl <nsanzumukizath@gmail.com>
2024-04-10 13:19:39 +0200

    summary content redifined

--- src/views/auxiliaire/summary.vue
+++ src/views/auxiliaire/summary.vue
@@ -1,33 +1,74 @@
 <template>
     <div class="summary">
-        <h3>Who is Thierry ?</h3>
-        <div style="text-align: justify; text-indent: 2em;">
-            <p>
-                He is a Business Strategist and Freelancer Fullstack Web Developer , studied Computer Science at ETS Kamenge  and graduated in 2016. 
-                First wrote his "hello world" in 2013. Forth in 2014, he discovered C programming language from the computer science course.  
-                Got addicted on Linux the same year, he could jump some course whereas reading Linux book in the library. 
-                He learned to Vim in 2015 and various UNIX commands as well as Bash scripting. 
+        <div>
+            <h3>About Me: Business Strategist and Fullstack Web Developer</h3>
+            <p class="textIndented">
+                I am a Burundian Business Strategist and Freelance Fullstack Web Developer, driven by a passion for 
+                strategic thinking and a deep-seated love for problem-solving. My academic journey at 
+                the University of Burundi honed my skills in the realm of business strategy, fostering my affinity for strategic planning, 
+                critical thinking, and collaborative solution-finding. This profound interest led me to nourish my strategic mindset, 
+                embracing the intricate art of philosophy and refining my ability in diagnosing complex scenarios.
+
+                On the technical front, I excel in the world of programming, boasting proficiency in Python, 
+                specifically in Django and Django Rest Framework, alongside expertise in Vue.js, HTML, and CSS. My practical abilities 
+                were evidenced through the successful completion of a Web App and an Android App for my esteemed inaugural client. 
+                As I continue to expand my horizons, I am currently engrossed in the development of my own website, 
+                geared towards showcasing my multifaceted skills and establishing a strong presence in the realm of freelancing.
+                This versatile blend of business strategy acumen and technical prowess empowers me to navigate 
+                the dynamic landscape with dexterity, embracing strategic thinking and technological innovation in equal measure.
+            </p>     
+        </div>
+        
+        <div>
+            <h3>Tech Passion and Learning Journey</h3>
+            <p class="textIndented">In 2014, my passion for Linux was ignited, leading me to explore the system extensively, 
+                often substituting formal courses with self-study using Linux books at the library. 
+                The year 2015 introduced me to Vim, UNIX commands, and Bash scripting, 
+                expanding my technical toolkit.
             </p>
-            <p>
-                Thanks to the Successful collaboration with his utmost friend Jonathan NKURUNZIZA(Github: INGANZAMARUMPU) 
-                since their first meeting in 2012 at the start of their studies, The friend has contributed the unleashed help 
-                and motivation to Thierry into computer science.
+        </div>
+
+        <div>
+            <h3>Collaborative Spirit with Jonathan NKURUNZIZA</h3>
+            <p class="textIndented">
+                My partnership with my close friend Jonathan NKURUNZIZA (Github: INGANZAMARUMPU) 
+                since 2012 has been instrumental in my growth in the field of computer science. Together, 
+                we have shared knowledge, motivation, and support, shaping my journey.
             </p>
-            <p>
-                After graduation in 2016, He worked on a big project which was written in C programming language in 2017. 
-                It is on that project that he was overwhelmed with the power on "POINTERS". 
-                He felt the joy of the feeling of touching the computer memory as he were entrusted such high responsibility( :-)reserved only and only for C/C++ programmers).
+        </div>
+
+        <div>
+            <h3>Professional Milestone: C Programming Project</h3>
+            <p class="textIndented">
+                A pivotal moment in my career was working on a significant project in 2017, focusing on C programming. 
+                This experience deepened my understanding of "POINTERS" and the responsibility 
+                that comes with navigating computer memory, a realm typically reserved for C/C++ experts.
             </p>
-            <p>
-                However, it turned to be in 2018 till 2021 that he switched into studying "Trade and Marketing" at University of Burundi. 
-                He needed this field to compensate the skills he needed. During that pursuit, He discovered interesting areas such as : 
-                Ethics, "Economie politique", "Comportement du Consommateur", Citizenship Values, "Administration Publique", Business&Administration Writings, 
-                "Principe du Marketing", and more related to Interpersonal Skills.
-                He felt also that he need to do something about "Developpement Personnel" as it is ongoing process of interconnecting 
-                his first Computer science's career and  Business Management. 
+        </div>
+
+        <div>
+            <h3>Diversification into Trade and Marketing</h3>
+            <p class="textIndented">
+                From 2018 to 2021, I ventured into studying Trade and Marketing at the University of Burundi to complement my skill set. 
+                This exploration introduced me to various crucial areas such as Ethics, Economics, Consumer Behavior, 
+                Citizenship Values, Public Administration, and Marketing Principles. 
+                It also emphasized the importance of personal development to bridge the gap between my technical and business expertise.
             </p>
+        </div>
+
+        <div>
+            <h3>Embracing Freelancing as a Fusion of Skills</h3>
             <p>
-                Thus He is able to respond to the call of Freelancing as  a  Business Strategist & Fullstack Developer.
+                With this diverse background and a blend of technical proficiency and business acumen, I am ready to address challenges 
+                as a Business Strategist and Fullstack Developer in the world of freelancing.
+            </p>
+        </div>
+
+        <div>
+            <h3>Engage with Me</h3>
+            <p>
+                Let's connect to explore synergies and opportunities for collaboration in the dynamic realms of technology, 
+                business strategy, and web development.
             </p>
         </div>
         
