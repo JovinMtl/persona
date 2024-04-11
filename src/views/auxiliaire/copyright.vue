973ea996346708198040b971c7eec0ae56efb00f
JovinMtl <nsanzumukizath@gmail.com>
2024-04-11 18:21:19 +0200

    copyright was moved sightly up

--- src/views/auxiliaire/copyright.vue
+++ src/views/auxiliaire/copyright.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="centered" style="font-size: 12px;margin-top: 20px;">
+    <div class="centered" style="font-size: 12px;margin-top: 20px;
+    margin-bottom: 5px;">
         <span style="font-weight: 900; 
         text-shadow: 2px -1px 6px #000, 2px 1px 16px #000;">
             Thierry Nsanzumukiza 
