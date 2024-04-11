9171036c0031bb5193d5cfba60a542bf14e87a7b
JovinMtl <nsanzumukizath@gmail.com>
2024-04-11 16:00:43 +0200

    bolded my name on the copyright

--- src/views/auxiliaire/copyright.vue
+++ src/views/auxiliaire/copyright.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="centered" style="font-size: 12px;margin-top: 20px;">
-        <span style="font-weight: 900;">Thierry Jovin Nsanzumukiza </span>
+        <span style="font-weight: 900; 
+        text-shadow: 2px -1px 6px #000, 2px 1px 16px #000;">
+            Thierry Nsanzumukiza 
+        </span>
         |{{year}}| 
-        <span style="font-weight: 800;">Bujumbura - Burundi</span> 
+        <span style="font-weight: 800;">Bujumbura - Burundi.</span> 
     </div>
 </template>
 <script>
