8b876fa1864d48e53439547e32b8f66b26d4a751
JovinMtl <nsanzumukizath@gmail.com>
2024-04-12 22:11:02 +0200

    renaming litteContact into contact

--- src/views/auxiliaire/welcome.vue
+++ src/views/auxiliaire/welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="centered welcomeText" style="font-size: 1.2rem;">
+    <div  class="centered welcomeText" style="font-size: 1.2rem;">
             <!-- <p>Bienvenue sur la Page officielle de Muteule.</p> -->
             <p>Welcome to the Official Website of Thierry.</p>
     </div>
