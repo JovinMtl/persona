3f85e92ec2a600207202640238f13a9b3d142680
JovinMtl <nsanzumukizath@gmail.com>
2024-04-14 09:03:48 +0200

    escaping portfolio for now, it should be displayed on it's proper page

--- src/views/layout/feuille.vue
+++ src/views/layout/feuille.vue
@@ -23,10 +23,12 @@
         <div class="centered">
           <my-offer></my-offer>
         </div>
+        <!-- <div class="centered">
+          <port-folio></port-folio>
+        </div> -->
         <div>
           <copy-right></copy-right>
         </div>
-        
         
       </ion-content>
     </ion-page>
@@ -36,8 +38,8 @@
   import { 
     IonContent, IonHeader, IonPage,
   } from '@ionic/vue';
-  import { ref } from 'vue'
-  import { useStore } from 'vuex'
+  // import { ref } from 'vue'
+  // import { useStore } from 'vuex'
   import menu from '../auxiliaire/menu.vue';
   import welcome from '../auxiliaire/welcome.vue'
   import showPic from '../operations/show-pic.vue'
@@ -45,6 +47,7 @@
   import summary from '../auxiliaire/summary.vue';
   import myOffer from '../auxiliaire/my-offer.vue';
   import copyright from '../auxiliaire/copyright.vue'
+  // import portFolio from '../auxiliaire/port-folio.vue';
   export default{
     components: {
       IonContent, IonHeader, IonPage,
@@ -55,6 +58,7 @@
       'summ-ary': summary,
       'my-offer': myOffer,
       'copy-right': copyright,
+      // 'port-folio': portFolio
     },
     setup(){
       // const store = useStore()
