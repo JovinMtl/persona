9d9ed2c49042a9456de89280d21c7cfd93da7b05
JovinMtl <nsanzumukizath@gmail.com>
2024-04-15 21:56:16 +0200

    can import portfolio dynamically

--- src/views/layout/feuille.vue
+++ src/views/layout/feuille.vue
@@ -2,34 +2,39 @@
     <ion-page>
       <ion-content>
         
-        <div>
-            <wel-come></wel-come>
-        </div>
+        <!-- <div>
+            <wel-come :title="title"></wel-come>
+        </div> -->
+        <div  class="centered welcomeText" style="font-size: 1.2rem;">
+              <!-- <p>Bienvenue sur la Page officielle de Muteule.</p> -->
+              <!-- <p>Welcome to the Official Website of Thierry.</p> -->
+              <p>{{ title }}</p>
+      </div>
         <!-- <div class="menuContainer">
             <show-pic></show-pic>
         </div> -->
         <div class="menuContainer menuSticky">
-            <me-nu></me-nu>
+            <me-nu @initial="actualMenu"></me-nu>
         </div>
-        <div class="menuContainer">
-            <show-pic></show-pic>
-        </div>
-        <div class="centered">
-            <open-pic></open-pic>
-        </div>
-        <div class="centered">
-          <summ-ary></summ-ary>
-        </div>
-        <div class="centered">
-          <my-offer></my-offer>
-        </div>
-        <!-- <div class="centered">
+        <div v-if="actual==1">
+            <div class="menuContainer">
+          <show-pic></show-pic>
+          </div>
+          <div class="centered">
+              <open-pic></open-pic>
+          </div>
+          <div class="centered">
+              <summ-ary></summ-ary>
+          </div>
+          <div class="centered">
+              <my-offer></my-offer>
+          </div> 
+        </div>   
+        <div v-if="actual==2" class="centered">
           <port-folio></port-folio>
-        </div> -->
-        <div>
-          <copy-right></copy-right>
+          
         </div>
-        
+        <copy-right></copy-right>
       </ion-content>
     </ion-page>
   </template>
@@ -38,29 +43,57 @@
   import { 
     IonContent, IonHeader, IonPage,
   } from '@ionic/vue';
-  // import { ref } from 'vue'
+  import { 
+    defineAsyncComponent, 
+    ref,
+    onBeforeUpdate } from 'vue'
   // import { useStore } from 'vuex'
   import menu from '../auxiliaire/menu.vue';
+  import home from '../pages/home.vue';
   import welcome from '../auxiliaire/welcome.vue'
   import showPic from '../operations/show-pic.vue'
   import openPic from '../operations/open-pic.vue'
   import summary from '../auxiliaire/summary.vue';
   import myOffer from '../auxiliaire/my-offer.vue';
   import copyright from '../auxiliaire/copyright.vue'
-  // import portFolio from '../auxiliaire/port-folio.vue';
+
+  const Porto = defineAsyncComponent(()=>import('../auxiliaire/port-folio.vue'))
   export default{
     components: {
       IonContent, IonHeader, IonPage,
       'me-nu': menu,
+      'hom-e' : home,
       'wel-come': welcome,
       'show-pic': showPic,
       'open-pic': openPic,
       'summ-ary': summary,
       'my-offer': myOffer,
       'copy-right': copyright,
-      // 'port-folio': portFolio
+      'port-folio': Porto,
     },
     setup(){
+      const actual = ref(1)
+      const title = ref('Welcome to the official Website of jove.')
+
+      const actualMenu = (value)=>{
+        if(value){
+          actual.value = Number(value)
+          // console.log("The actual Menu is: ", actual.value)
+          if(actual.value==1){
+          title.value='Welcome to the Official Website of Thierry.'
+          // console.log("Setting title to A")
+        } else if(actual.value==2){
+          title.value="You are seeing my achievements"
+          // console.log("Setting title to B")
+        }
+        }
+      }
+
+      onBeforeUpdate(()=>{
+        actualMenu()
+        console.log("The actual Value is : ", actual.value)
+        
+      })
       // const store = useStore()
       // const viewportWidth = ref(window.innerWidth);
       // const updateViewportWidth = () => {
@@ -70,39 +103,12 @@
       //   window.addEventListener('resize', updateViewportWidth);
       // });
       return {
-        // viewportWidth
+        // viewportWidth,
+        title,
+        actual,
+        actualMenu,
       }
     }
   }
   </script>
-  
-  <style scoped>
-  #container {
-    text-align: center;
-    
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-  
-  #container strong {
-    font-size: 20px;
-    line-height: 26px;
-  }
-  
-  #container p {
-    font-size: 16px;
-    line-height: 22px;
-    
-    color: #8c8c8c;
-    
-    margin: 0;
-  }
-  
-  #container a {
-    text-decoration: none;
-  }
-  </style>
   
