c3d39fb5bd82eb77064feeb4b8ed4b58e0cb8cb0
JovinMtl <nsanzumukizath@gmail.com>
2024-04-15 22:01:31 +0200

    and portfolio componet was created

---
+++ src/views/pages/portofolio.vue
@@ -0,0 +1,17 @@
+<template>
+        <div>
+            <p>My portfolio is this one</p>
+        </div>
+    
+</template>
+<script>
+import { IonPage, IonContent,} from '@ionic/vue'
+export default {
+    components:{
+        IonPage, IonContent,
+    },
+    setup() {
+        
+    },
+}
+</script>
