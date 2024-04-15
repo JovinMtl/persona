bfab38c8b69bd4d688c63e39883e1a4bd903eb8f
JovinMtl <nsanzumukizath@gmail.com>
2024-04-15 22:00:14 +0200

    have to display title dynamically

--- src/views/auxiliaire/welcome.vue
+++ src/views/auxiliaire/welcome.vue
@@ -1,6 +1,7 @@
 <template>
     <div  class="centered welcomeText" style="font-size: 1.2rem;">
             <!-- <p>Bienvenue sur la Page officielle de Muteule.</p> -->
-            <p>Welcome to the Official Website of Thierry.</p>
+            <!-- <p>Welcome to the Official Website of Thierry.</p> -->
+            <p>{{ title }}</p>
     </div>
 </template>
