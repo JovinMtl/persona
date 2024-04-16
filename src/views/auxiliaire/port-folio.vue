98f1ecf8a2983903b894813b6da434ec06230286
JovinMtl <nsanzumukizath@gmail.com>
2024-04-16 17:44:45 +0200

    have updated the photos

--- src/views/auxiliaire/port-folio.vue
+++ src/views/auxiliaire/port-folio.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
-        <div style="display: block; position: relative;">
-            Here is my Portfolio
+        <div style="display: block; position: relative;
+        margin-top: 20px;">
+            <!-- Here is my Portfolio -->
         </div>
     </div>
     
     <div style="display: inline-block;" v-for="proof in proofs">
         <div class="portoContent">
             <img :src="proof.image" class="imgProofCss"/>
-            {{ proof.title }}.
+            <div class="textCss">{{ proof.description }}.</div> 
         </div>
     </div>
     
@@ -20,57 +21,42 @@ export default {
         const proofs = [
             {
                 'title':'title one',
-                'image':'/personaFoto/jove20.png',
-                'description':''
+                'image':'/personaFoto/jove1.png',
+                'description':'I am a Burundian Business Strategist and Freelance Fullstack Developer, driven by a passion for strategic thinking and a deep-seated love for problem-solving. My academic journey at the University of Burundi honed my skills in the realm of business strategy, fostering my affinity for strategic planning, critical thinking, and collaborative solution-finding.'
             },
             {
                 'title':'title two',
-                'image':'/personaFoto/jove21.png',
+                'image':'/personaFoto/jove2.png',
                 'description': ''
             },
             {
                 'title':'',
-                'image':'/personaFoto/jove22.png',
+                'image':'/personaFoto/jove3.png',
                 'description': ''
             },
             {
                 'title':'title two',
-                'image':'/personaFoto/jove23.png',
+                'image':'/personaFoto/jove4.png',
                 'description': ''
             },
             {
                 'title':'',
-                'image':'/personaFoto/jove24.png',
+                'image':'/personaFoto/jove5.png',
                 'description': ''
             },
             {
                 'title':'title two',
-                'image':'/personaFoto/jove25.png',
+                'image':'/personaFoto/jove6.png',
                 'description': ''
             },
             {
                 'title':'',
-                'image':'/personaFoto/jove26.png',
+                'image':'/personaFoto/jove7.png',
                 'description': ''
             },
             {
                 'title':'title two',
-                'image':'/personaFoto/jove27.png',
-                'description': ''
-            },
-            {
-                'title':'',
-                'image':'/personaFoto/jove28.png',
-                'description': ''
-            },
-            {
-                'title':'title two',
-                'image':'/personaFoto/jove29.png',
-                'description': ''
-            },
-            {
-                'title':'',
-                'image':'/personaFoto/jove30.png',
+                'image':'/personaFoto/jove8.png',
                 'description': ''
             },
         ];
@@ -84,26 +70,46 @@ export default {
 
 <style scoped>
 .imgProofCss{
-    border: 1px solid red;
+    /* border: 1px solid red; */
     border-top-left-radius: 25px; 
     border-top-right-radius: 25px;
 }
+.textCss{
+    color: white;
+    padding: 10px 10px;
+    text-align: justify;
+}
 .portoContent{
     width: 40vw; 
-    border-radius: 25px 25px 0px 0px;
+    /* border-radius: 25px 25px 0px 0px; */
+    border-radius: 25px;
     background-color: gray; 
     text-align: center; 
     border: 1px solid gray;
     margin: 20px 20px;
+    /* text-align: justify; */
 }
 @media screen and (max-width: 600px) {
-.portoContent{
+/* .portoContent{
     width: 80vw; 
     border-radius: 25px 25px 0px 0px;
+    border-radius: 25px;
     background-color: gray; 
     text-align: center; 
     border: 1px solid gray;
     margin: 20px 20px;
+    padding: 10px 10px;
+    text-align: justify;
+} */
+.portoContent{
+    width: 80vw; 
+    /* border-radius: 25px 25px 0px 0px; */
+    border-radius: 25px;
+    background-color: gray; 
+    text-align: center; 
+    border: 1px solid gray;
+    margin: 20px 20px;
+    /* text-align: justify; */
 }
 }
 </style>
