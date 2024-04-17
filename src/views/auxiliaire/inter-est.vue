370aec2a2227b857e3df40610c4503592cfb2c6d
JovinMtl <nsanzumukizath@gmail.com>
2024-04-17 15:14:43 +0200

    setting a margin for my saying in the interest component

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <p>
-            "I have not yet provided my interests yet. Soon, I will provide it.", says Thierry Jovin.
+        <p style="margin: 20px 20px;">
+            "I have not yet provided my interests yet. Soon, I will provide it.", 
+            says Thierry Jovin.
         </p>
     </div>
 </template>
