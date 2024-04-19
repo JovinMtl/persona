64658eb80595b4d94a991ce2f8de02316acbd467
JovinMtl <nsanzumukizath@gmail.com>
2024-04-19 15:46:34 +0200

    info can be adapted on most screen size

--- src/views/auxiliaire/info.vue
+++ src/views/auxiliaire/info.vue
@@ -3,9 +3,9 @@
         <!-- <div>Contact</div> -->
         
         <div class="infoContainer">
-            <div class="infoText" style="background-color: transparent;
-            width: 80%;">
-                <div class="makeList">
+            <div class="infoText">
+                <div class="textContainer">
+                    <div class="makeList">
                     <a class="logoColor" href="http://wa.me/+25779071453">
                         <ion-icon :src="personOutline" class="logoLarge"></ion-icon>
                     </a>
@@ -30,7 +30,7 @@
                         Bujumbura  -  Burundi
                     </span>   
                 </div>
-                <div class="makeList">
+                <div class="makeList gmailClass">
                     <a class="logoColor" href="mailto:nsanzumukizath@gmail.com">
                         <ion-icon :src="mailOutline" class="logoLarge"></ion-icon>
                     </a>
@@ -62,16 +62,15 @@
                         muteule
                     </span> 
                 </div>
+                </div>
+                
             </div>
-            <div class="infoImage" style="">
-                <span style="border-radius: 50px;">
-                    <!-- <ion-img src="/personaFoto/loader.png"/> -->
-                    <img style="border-radius: 150px;" src="/personaFoto/profile.png" alt="">
+            <div class="infoImage" >
+                <span style="border-radius: 50px;
+                ">
+                    <img style="border-radius: 150px;
+                    box-shadow: 0 0 25px gray;" src="/personaFoto/profile.png" alt="">
                 </span>
-
-                <span class="cicleProfile" style="">
-                </span>
-            
             </div>
             
             
@@ -101,63 +100,174 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap');
+/* @import url('https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap'); */
 @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
 
 
+
+ 
 .infoContainer{
     width: 58vw; 
     height: auto; 
-    /* display: inline-block; */
     display: inline-flex;
-    background-color: transparent;
+    /* background-color: transparent; */
     /* background-color: salmon;  */
+    background: linear-gradient(to right, #f04f4f,#f08080, #ff8c69);
     padding: 1.5vh 10vw;
-    padding: 1.5vh 5vw;
+    padding: 1.5vh 2vw;
     border-radius: 25px; 
     border: 12px solid gray;
+    /* border: 12px solid #575353; */
     text-align: left
 }
-.infoImage{
+.infoText{
     background-color: transparent;
-    position: relative;
-    --width:48%;
-    width: 25%; 
-    padding-top: 25%; 
-    display: block;
+    width: 80%;
 }
-.cicleProfile{
-    display: inline-flex; 
-    position: absolute ;
-    /* border: 3px double salmon; */
-    /* border: 3px groove salmon; */
-    box-shadow: 0 0 25px grey;
-    width: 100%; height: var(--width);
-    border-radius: 150px;
-    left: 0%;
+.textContainer{
+    margin-top: 0px;
+    /* background-color: green; */
+}
+.infoImage{
+    /* background-color: red; */
+    position: relative;
+    width: 25%; 
+    display: flex;
+    align-items: flex-end;
 }
 .logoLarge{
     font-size: xx-large;
 }
 .logoColor{
     color: black;
-    text-shadow: 3px 3px 4px red;
 }
 .makeList{
-    display: block;
-    margin: 5px 5px;
+    width: 100%;
+    height: auto;
+    display: inline-flex;
+    margin: 5px 45px;
+    /* padding: 0px 10px; */
+    align-content: center;
+    align-items: center;
+    /* background-color: yellow; */
 }
 .textAdjust{
     position: relative;
-    top: -9px; 
+    top: -1px; 
     left: 20px; 
     left: 30px;
-    /* font-weight: 700; */
     font-size: x-large;
     /* color: white; */
     /* font-family: 'Open Sans', sans-serif; */
     font-family: Sen, sans-serif;
     font-family: Rubik;
 }
+
+
+@media screen and (max-width: 565px){
+    .infoContainer{
+    width: 80vw; 
+    height: auto; 
+    display: inline-flex;
+    /* background-color: transparent; */
+    /* background-color: salmon;  */
+    background: linear-gradient(to right, #f04f4f,#f08080, #ff8c69);
+    padding: 1.5vh 10vw;
+    padding: 1.5vh 2vw;
+    border-radius: 25px; 
+    border: 12px solid gray;
+    /* border: 12px solid #575353; */
+    text-align: left
+}
+.infoText{
+    background-color: transparent;
+    width: 100%;
+}
+.textContainer{
+    margin-top: 25vw;
+    margin-left: 1vw;
+    /* background-color: green; */
+    display: block;
+}
+.makeList{
+    /* width: 100%; */
+    width: 100vw;
+    height: auto;
+    /* display: inline-flex; */
+    display: block;
+    /* margin: 5px 5px; */
+    /* margin-left: 20px; */
+    /* padding: 0px 10px; */
+    align-content: center;
+    align-items: center;
+    /* background-color: yellow; */
+}
+.infoImage{
+    /* background-color: red; */
+    position: absolute;
+    /* width: 25%;  */
+    width: 25%;
+    height: 25vw;
+    display: flex;
+    align-items: flex-end;
+    left: 36vw;
+}
+}
+
+@media screen and (max-width: 815px) {
+.makeList{
+    width: 100%;
+    height: auto;
+    display: inline-flex;
+    margin: 5px 5px;
+    /* padding: 0px 10px; */
+    align-content: center;
+    align-items: center;
+    /* background-color: yellow; */
+}
+.textAdjust{
+    position: relative;
+    top: -1px; 
+    left: 20px; 
+    left: 10px;
+    font-size: large;
+    /* font-size: 22px; */
+    font-family: Rubik;
+}
+
+@media screen and (max-width: 430px){
+.infoContainer{
+    width: 84vw !important; 
+}
+.textAdjust{
+    left: 2px !important;
+    font-size: 17px;
+}
+.infoImage{
+    left: 38vw !important;
+}
+}
+
+@media screen and (max-width: 360px){
+.textAdjust{
+    position: relative;
+    top: -1px; 
+    left: 20px; 
+    left: 10px;
+    font-size: small;
+    /* font-size: 22px; */
+    font-family: Rubik;
+}
+}
+
+@media screen and (max-width: 294px){
+.textAdjust{
+    font-size: 11px;
+}
+}
+
+
+}
+
 </style>
 
