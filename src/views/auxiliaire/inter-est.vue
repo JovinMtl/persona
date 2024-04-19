d80e62b10dd9c577752bd519ab30e23be46f1fd1
JovinMtl <nsanzumukizath@gmail.com>
2024-04-19 17:34:47 +0200

    correcting the grammar of waiting message

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -2,7 +2,7 @@
     <div>
         <p style="margin-top: 40px; margin-bottom: 67vh;">
            <span style="font-family: Cantarell; color: brown;">
-            "I have not yet provided my interests here. But soon, I will provide it.", 
+            "I have not yet provided my interests here. But soon, I will provide them.", 
            </span> 
             says Thierry Jovin.
         </p>
