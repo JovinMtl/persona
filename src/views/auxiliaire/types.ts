cc50885c29ddd31dee5bebf6b4cfd90b844f6a81
JovinMtl <nsanzumukizath@gmail.com>
2024-05-12 20:11:14 +0200

    Offer interface is defined and is ready to be used in offer-content component

--- src/views/auxiliaire/types.ts
+++ src/views/auxiliaire/types.ts
@@ -1,2 +1,9 @@
 // This file will contain type and interfaces to be used
 // accross  the entire project
+
+
+export interface Offer {
+    title : string;
+    content : string;
+    color : string;
+}
