52482e3bf6517897969675a475e3c4be94000105
JovinMtl <nsanzumukizath@gmail.com>
2024-06-27 10:25:01 +0200

    removing the commented code

--- src/views/auxiliaire/copyright.vue
+++ src/views/auxiliaire/copyright.vue
@@ -1,10 +1,6 @@
 <template>
     <div class="centered" style="font-size: 12px;margin-top: 20px;
     margin-bottom: 10px;">
-        <!-- <span style="font-weight: 900; 
-        text-shadow: 2px -1px 6px rgb(56, 54, 54), 2px 1px 16px rgb(56, 54, 54);">
-            Thierry Nsanzumukiza 
-        </span> -->
         <span style="font-weight: 900; 
         text-shadow: 2px -1px 3px rgb(56, 54, 54), 2px 1px 8px rgb(56, 54, 54);">
             Thierry Nsanzumukiza 
