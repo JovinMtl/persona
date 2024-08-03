c872db8a1643a215775a9addcdf64429d8774c98
JovinMtl <nsanzumukizath@gmail.com>
2024-08-03 18:26:14 +0200

    animating brief

--- src/views/auxiliaire/brief.vue
+++ src/views/auxiliaire/brief.vue
@@ -13,5 +13,20 @@
     position: relative;
     top: 84vh;
     left: 30vw;
+    animation-name: meme;
+    animation-duration: 110ms;
+}
+
+@keyframes meme {
+    from{
+        width: 50vw;
+        height: 25vh;
+        top: 78vh;
+        left: 25vw;
+    }
+    to{
+        width: 40vw;
+        height: 15vh;
+    }   
 }
 </style>
