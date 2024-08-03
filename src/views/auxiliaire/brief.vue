6a8ae8148027e7ce385022a3c073ed30c2c16340
JovinMtl <nsanzumukizath@gmail.com>
2024-08-03 17:20:06 +0200

    brief component implemented.

---
+++ src/views/auxiliaire/brief.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mainContainer">
+        Ici le briefing
+    </div>
+</template>
+
+<style scoped>
+.mainContainer{
+    width: 20vw;
+    height: 5vw;
+    background-color: brown;
+}
+</style>
