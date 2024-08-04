5f761e3e29985e007ce008614334b98bc36beb35
JovinMtl <nsanzumukizath@gmail.com>
2024-08-04 16:59:38 +0200

    added close icon

--- src/views/auxiliaire/brief.vue
+++ src/views/auxiliaire/brief.vue
@@ -5,12 +5,17 @@
             <!-- <span class="b m-2 s-2">6</span> front-end projects + <span class="b m-2 s-2">2</span> back-end projects = 7 months. -->
             6 front-end projects and 2 back-end projects in 7 months.
             <div class="contrParent">
-                <div class="cloControl"></div>
+                <div class="cloControl">
+                    <ion-icon :src="close"></ion-icon>
+                </div>
             </div>
         </div>
     </div>
 
 </template>
+<script setup lang="ts">
+import { close} from 'ionicons/icons'
+</script>
 
 <style scoped>
 .contrParent{
