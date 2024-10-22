5b97a6a91946639be15402b99c8db4fba9524559
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-22 10:13:19 +0200

    applying big-quote class on both quote.

--- src/views/operations/open-pic.vue
+++ src/views/operations/open-pic.vue
@@ -13,7 +13,8 @@
                 <span style="font-size=5px;"> -- Th. Jov. Nsanzumukiza</span>
             </span> -->
             <blockquote class="quote">
-                <span class="big-quote">“</span>Before downloading one file, please delete two.” 
+                <span class="big-quote">“</span>Before downloading one file, please delete two.
+                <span class="big-quote">“</span> 
                 <span class="author">— Th. Jov. Nsanzumukiza</span>
             </blockquote>
 
