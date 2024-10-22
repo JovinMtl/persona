95f9c3cd55cbaa0d4725b46badc903943e8c9f3f
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-22 16:29:47 +0200

    making flex the interest container

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="padding: 30px;">
+    <div style="padding: 30px; display: flex;">
         <!--
         <p style="margin-top: 40px; margin-bottom: 67vh;">
            <span style="font-family: Cantarell; color: brown;">
