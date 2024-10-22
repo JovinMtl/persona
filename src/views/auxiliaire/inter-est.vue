a0e33a16c2a737ce7a6ab5b29a6dc73afe2a2027
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-22 09:07:35 +0200

    hidding the initial message in interests

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
+        <!--
         <p style="margin-top: 40px; margin-bottom: 67vh;">
            <span style="font-family: Cantarell; color: brown;">
             "I have not yet provided my interests here. But soon, I will provide them.", 
            </span> 
             says Thierry Jovin.
         </p>
+        -->
     </div>
 </template>
