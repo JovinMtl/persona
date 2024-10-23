9e73b62cddd50769c1f7b555077daa018e92ae85
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-23 11:28:26 +0200

    encircling the author's name

--- src/views/auxiliaire/copyright.vue
+++ src/views/auxiliaire/copyright.vue
@@ -2,7 +2,8 @@
     <div class="centered" style="font-size: 12px;margin-top: 20px;
     margin-bottom: 10px;">
         <span style="font-weight: 900; 
-        text-shadow: 2px -1px 3px rgb(56, 54, 54), 2px 1px 8px rgb(56, 54, 54);">
+        text-shadow: 2px -1px 3px rgb(56, 54, 54), 2px 1px 8px rgb(56, 54, 54);
+            padding:20px; border:1px solid black;">
             Thierry Nsanzumukiza 
         </span>
         |{{year}}| 
