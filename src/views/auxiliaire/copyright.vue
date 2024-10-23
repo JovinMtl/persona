d491bc6abd08747cbe1f19783b192e08c96e93d9
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-23 13:37:41 +0200

    removing the font-family on the author's name

--- src/views/auxiliaire/copyright.vue
+++ src/views/auxiliaire/copyright.vue
@@ -2,10 +2,11 @@
     <div class="centered" style="font-size: 12px;margin-top: 20px;
     margin-bottom: 10px;">
        <!-- text-shadow: 2px -1px 3px rgb(56, 54, 54), 2px 1px 8px rgb(56, 54, 54); -->
-        <span class="author">
+        <span style="font-weight: 900; 
+            padding:5px; border:1px solid black; border-radius:3px;">
             Thierry Nsanzumukiza 
         </span>
-        <span style="margin: 0 5px;">
+        <span class="author" style="margin: 0 5px;">
         {{year}}
         </span>
         <span style="font-weight: 800;padding:5px; border:1px solid black; 
@@ -28,9 +29,6 @@ export default defineComponent ({
 <style scoped>
     @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap')
     .author{
-        font-weight: 900; 
-        padding:5px; 
-        border:1px solid black; border-radius:3px;
         font-family:Montserrat;
     }
 </style>
