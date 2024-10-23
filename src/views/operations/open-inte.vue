3c6b18f72ec79f35fa17459cf43cb8ff0171a382
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-23 15:45:22 +0200

    Initial content

--- src/views/operations/open-inte.vue
+++ src/views/operations/open-inte.vue
@@ -1,1 +1,13 @@
+<template>
+  <div class="inte">
+    Merci de voir mon interet personnel
+  </div>
+</template>
 
+<style scoped>
+  .inte{
+    width: 40vw;
+    height: 200px;
+    background-color: red;
+  }
+</style>
