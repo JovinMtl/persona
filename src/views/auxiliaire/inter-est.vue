2c813cb560758a7ace90d7cdc9e4d4ea43e605da
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-24 11:32:56 +0200

    removing the old commented code

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -1,13 +1,5 @@
 <template>
     <div style="padding: 30px;">
-        <!--
-        <p style="margin-top: 40px; margin-bottom: 67vh;">
-           <span style="font-family: Cantarell; color: brown;">
-            "I have not yet provided my interests here. But soon, I will provide them.", 
-           </span> 
-            says Thierry Jovin.
-        </p>
-        -->
         <div class="intere" id="1" @click=mentionClicked>Digital Discipline</div>
         <div class="intere">Ubuzima Butyoroye</div>
         <teleport to="body">
@@ -25,7 +17,6 @@
         openInterst.value = true;
     }
     const closeInterst = ()=>{
-        console.log("Closing the popup...");
         openInterst.value = false;
     }
 </script>
