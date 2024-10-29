6f548fc11d52c03eb78cb0a995597702b332bd37
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-29 16:40:42 +0100

    updating the detail of alchol without you

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -35,8 +35,8 @@
       'content': 'Alchol without you',
       'detail' : `I wrote about the possibility of getting free from want to drink any drink 
           that is regarded as <b>special</b>. It's about getting free from drinking alcohol or soft drink without religious 
-        implication, but to drink on casual need. 
-        I shared on how I started to find it were joyful for me to be independent on 
+        implication, but to drink only by casual need. 
+        I shared on how I found it were joyful to be independent on 
         alcohol while staying <i>Catholic</i>. 
         However, I reserved my right to take any of them only on honorific request.<br><br>
         <span class='autho'>-- Th. Jov. Nsanzumukiza</span>
