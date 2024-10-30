31f2a8e1927eedd1d0b60651ae3940a6ad17c8b8
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-30 22:02:21 +0200

    emitting only endSignal

--- src/views/operations/open-inte.vue
+++ src/views/operations/open-inte.vue
@@ -10,8 +10,8 @@
       </span>
     </div>
     <div class="f-l w-i cl">
-      <div class="btn sp" v-if="inteContent.notice" @click="signalEndF">No</div>
-      <div class="btn" @click="signalEnd">
+      <div class="btn sp" id='1' v-if="inteContent.notice" @click="signalEnd">No</div>
+      <div class="btn" id='2' @click="signalEnd">
         Ok
       </div>
     </div>
@@ -23,11 +23,13 @@
   const emit =  defineEmits(['endSignal', 'endSignalF'])
   const inteContent = inject('interestShow')
 
-  const signalEnd = ()=>{
-    emit('endSignal')
-  }
-  const signalEndF = ()=>{
-    emit('endSignalF')
+  const signalEnd = (e)=>{
+    let index = e.target.id;
+    if (index == 1){
+        emit('endSignal', 1)
+    } else if (index == 2){
+        emit('endSignal', 2)
+}
   }
 
   console.log("The actualInterest injected is: ", inteContent)
