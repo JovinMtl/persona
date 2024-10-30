bd28a4879c9d1da00f84e3e3645d6ac21828b2d6
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-10-30 20:00:06 +0200

    removing styles for notice paragraph

--- src/views/auxiliaire/inter-est.vue
+++ src/views/auxiliaire/inter-est.vue
@@ -29,7 +29,7 @@
         'title': 'notice',
         'detail': `
         <span class="tl">Notice</span>
-        <p style="margin: -10px 0">I appreciate your journey so far and 
+        <p>I appreciate your journey so far and 
         hope you can find something we have in common. But please don't judge me
         for sharing my intimate interests with you.</p>
         <p class='fm'>Do you agree to keep this neutral for my consideration?</p>`,
