44ff0c275148174480077044081d728729be591a
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-11-02 09:00:39 +0100

    CSS optimization

--- src/views/operations/show-pic.vue
+++ src/views/operations/show-pic.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="centered welcomeText bigWidth">
-        <div style="margin-top: 20px; font-weight: bolder;">
+        <div class="mt-20 fw-b">
             Nsanzumukiza Thierry Jovin
         </div> 
        
-        <div style="margin: 20px 20px; color: brown; 
-        font-family: 'Open Sans', sans-serif; font-weight: 100;">
+        <div class="m-20 c-b ff-o">
             Discover Thierry Nsanzumukiza, a Business Strategist and 
             Full Stack Developer based in 
             <span style="font-weight: 700;">Burundi</span> . 
             
             I love thinking, and happy to contribute to the Business strategy. 
-            And on the other side to have a happy coding in  Python 
+            And on the other side, to have a happy coding in  Python 
             (Django & DRF), JavaScript (Vue.js + Ts), HTML and CSS.
         </div>
     </div>
 </template>
+
+<style>
+    .mt-20{margin-top: 20px;}
+    .fw-b{font-weight: bolder;}
+    .m-20{margin: 20px;}
+    .c-b{color: brown;}
+    .ff-o{font-family: 'Open Sans', sans-serif; font-weight: 100;}
+</style>
