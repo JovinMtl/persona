5093413b6e1f7420a5581cbdb5f5b21208715dd0
Nsanzumukiza Thierry <nsanzumukizath@gmail.com>
2024-11-08 09:42:52 +0100

    replacing the actual profile with a light weight version

--- src/views/auxiliaire/info.vue
+++ src/views/auxiliaire/info.vue
@@ -83,7 +83,7 @@
                 <span style="border-radius: 50px;
                 ">
                     <img style="border-radius: 150px;
-                    box-shadow: 0 0 25px gray;" src="/personaFoto/profile.jpg" alt="">
+                    box-shadow: 0 0 25px gray;" src="/personaFoto/muteule-profile.jpg" alt="">
                 </span>
             </div> 
         </div>
