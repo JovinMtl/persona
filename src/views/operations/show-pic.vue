cc0a585d5708f3590e49dccbef848efdf26f3ffb
JovinMtl <nsanzumukizath@gmail.com>
2024-12-14 13:27:38 +0200

    updating my presentation

--- src/views/operations/show-pic.vue
+++ src/views/operations/show-pic.vue
@@ -5,13 +5,11 @@
         </div> 
        
         <div class="m-20 c-b ff-o">
-            Discover Thierry Nsanzumukiza, a Business Strategist and 
-            Full Stack Developer based in 
-            <span style="font-weight: 700;">Burundi</span> . 
+            Discover Thierry, a  Full Stack Developer based in 
+            <span style="font-weight: 700;">Burundi</span>
+            with 5+ years of experience in programming.
             
-            I love being a thinker and having a winning strategy that increases a company's market share.
-
-On the other hand, my thinking gives me the ability to solve problems in the form of algorithms and my knowledge of Python (Django & DRF), JS (Vue.js + Ts), HTML and CSS.
+            Caring about good solution, satisfaction and happy coding in Python, Django & DRF, Vue.js, HTML and CSS.
         </div>
     </div>
 </template>
