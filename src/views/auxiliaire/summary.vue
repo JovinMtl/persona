222cef17b852744cb0f36db3cdafbf73888d9578
JovinMtl <nsanzumukizath@gmail.com>
2025-01-08 15:05:34 +0200

    updated my reason to studying commerce and marketing

--- src/views/auxiliaire/summary.vue
+++ src/views/auxiliaire/summary.vue
@@ -94,10 +94,13 @@ Same my passion for Linux was ignited, leading me to explore the system extensiv
             }, 
             {
                 'title':'Diversification into Commerce and Marketing',
-                'content':`From 2018 to 2021, I ventured into studying Commerce and Marketing at the University of Burundi to complement my skill set. 
-                This exploration introduced me to various crucial areas such as Ethics, Economics, Consumer Behavior, 
-                Citizenship Values, Public Administration, and Marketing Principles. 
-                It also emphasized the importance of personal development to bridge the gap between my technical and business expertise.
+                'content':`From 2018 to 2021, I ventured to study commerce and marketing at the University of Burundi to complement my skills. 
+                            Just before, I was locked inside myself, a prisoner of the computer world. On the other hand, I was frustrated 
+                            that I did not have the soft skills to deal with myself. Like leadership and business acumen, like those who 
+                            went to business school. 
+                            This exploration introduced me to several crucial areas such as ethics, economics, consumer behaviour, civic values, 
+                            public administration, business strategy and marketing principles. It also emphasised the importance of 
+                            personal development to bridge the gap between my technical and business expertise. 
            `,
                 'detail':false
             },
